bfa6f03733f1a026d8eae92db893ef1485b94518
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-05-03 12:24:02 +0300

    added reduce polyfill

--- myPolyfills/index.js
+++ myPolyfills/index.js
@@ -13,6 +13,24 @@ module.exports = function () {
         result.push(callback(this[i], i, this));
       }
       return result;
+    };
+  }
+
+  if (!Array.prototype.myReduce) {
+    Array.prototype.myReduce = function(callback, initValue) {
+      if (!(this instanceof Array || this instanceof String)) {
+        throw new TypeError('Array.prototype.myReduce was called on wrong type')
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError(`Array.prototype.myReduce ${callback} is not a function`)
+      }
+      
+      let acc = arguments.length >= 2 ? initValue : this[0];
+      let isStart = arguments.length >= 2 ? 0 : 1;
+      for(let i = isStart; i < this.length; i++) {
+        acc = callback(acc, this[i], i, this)
+      }
+      return acc;
     }
   }
 }
