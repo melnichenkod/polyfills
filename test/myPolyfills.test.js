6a01a6f8653a781b8651bcdbd1ba8d80d99d6344
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-05-05 11:52:46 +0300

    added tests for reduce polifill

--- test/myPolyfills.test.js
+++ test/myPolyfills.test.js
@@ -47,6 +47,57 @@ describe('Testing myPolyfills', () => {
     });
 
     
-  }
-  )
-})
+  });
+  describe('Testing myReduce', () => {
+    it('Check - wrong type', () => {
+      function badFn() {
+        Array.prototype.myReduce.call(true, (acc, value) => acc + value)
+      };
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - no callback', () => {
+      function badFn() {
+        [1, 2, 3].myReduce();
+      };
+      expect(badFn).to.throw(TypeError);
+    });
+
+    it('Check - sum', () => {
+      const arr = [1, 2, 3, 4];
+      expect(
+        arr.myReduce((acc, value) => acc + value)
+      ).to.equal(
+        arr.reduce((acc, value) => acc + value)
+      );
+    });
+
+    it('Check - string', () => {
+      expect(
+        Array.prototype.myReduce.call('test', (acc, value) => {
+          acc[value] = value + 1;
+          return acc;
+        }, {})
+      ).to.deep.equal(
+        Array.prototype.reduce.call('test', (acc, value) => {
+          acc[value] = value + 1;
+          return acc;
+        }, {})
+      )
+    })
+
+    it('Check - properties', () => {
+      expect(
+        [1, 2, 3, 4].myReduce((acc, value, index, arr) => {
+          acc.push([value, index, arr]);
+          return acc;
+        }, [])
+      ).to.deep.equal(
+        [1, 2, 3, 4].reduce((acc, value, index, arr) => {
+          acc.push([value, index, arr]);
+          return acc;
+        }, [])
+      );
+    });
+  });
+});
